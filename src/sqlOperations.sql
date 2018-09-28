a0f7c1ea52ade58d263b3d5b57178b6ecaf5bfd9
Sarmad <sarmadsa2@gmail.com>
2018-09-28 22:21:26 +0200

    Solve Q3

--- src/sqlOperations.sql
+++ src/sqlOperations.sql
@@ -7,3 +7,9 @@ FROM Movie m, Rating r
 WHERE r.mID = m.mID
   AND r.stars >= 4
 ORDER BY m.year
+
+--Q3
+SELECT DISTINCT m.title
+From Movie m
+LEFT JOIN Rating r ON m.mID = r.mID
+WHERE r.mID IS NULL
