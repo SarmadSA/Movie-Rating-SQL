aca7bfd4d00ca9c6367c90239f4b9e3a577c99fb
Sarmad <sarmadsa2@gmail.com>
2018-09-29 00:26:56 +0200

    Solve Q2 Extras

--- src/sqlOperationsExtras.sql
+++ src/sqlOperationsExtras.sql
@@ -4,3 +4,9 @@ FROM Rating r, Reviewer re, Movie m
 WHERE m.title = 'Gone with the Wind'
   AND r.rID = re.rID
   AND r.mID = m.mID
+
+--Q2
+SELECT r.name, m.title, ra.stars
+FROM Reviewer r, Movie m, Rating ra
+WHERE r.rID = ra.rID AND ra.mID = m.mID
+AND m.director = r.name
