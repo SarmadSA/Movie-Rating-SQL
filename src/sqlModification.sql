a424bc0b157e7b0294f57b6924636acafffc4a83
Sarmad <sarmadsa2@gmail.com>
2018-09-30 17:42:02 +0200

    Solve Q4 Modification

--- src/sqlModification.sql
+++ src/sqlModification.sql
@@ -14,3 +14,7 @@ WHERE mID IN (SELECT DISTINCT m.mID
               WHERE (SELECT AVG(rr.stars)
                      FROM Rating rr
                      WHERE rr.mID = m.mID) >=4)
+
+--Q4
+DELETE FROM Rating
+WHERE stars < 4 AND mID IN (SELECT mID FROM Movie WHERE (year < 1970 OR year > 2000))
