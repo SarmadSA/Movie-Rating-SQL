1f7ba6f1bdb7ac2fc09f7227af84071d3fb9dfcf
Sarmad <sarmadsa2@gmail.com>
2018-09-30 17:41:43 +0200

    Solve Q3 Modification

--- src/sqlModification.sql
+++ src/sqlModification.sql
@@ -5,3 +5,12 @@ VALUES('Roger Ebert', 209)
 --Q2
 INSERT INTO Rating(rID,mID,stars)
 SELECT (SELECT rID FROM Reviewer WHERE name = 'James Cameron') , mID, 5 FROM Movie
+
+--Q3
+UPDATE Movie
+SET year = year + 25
+WHERE mID IN (SELECT DISTINCT m.mID
+              FROM Movie m
+              WHERE (SELECT AVG(rr.stars)
+                     FROM Rating rr
+                     WHERE rr.mID = m.mID) >=4)
