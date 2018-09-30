7b8d1f980c46fb5d6364eba39bc39374d37c2efd
Sarmad <sarmadsa2@gmail.com>
2018-09-30 17:41:15 +0200

    Solve Q2 Modification

--- src/sqlModification.sql
+++ src/sqlModification.sql
@@ -1,3 +1,7 @@
 --Q1
 INSERT INTO Reviewer(name, rID)
 VALUES('Roger Ebert', 209)
+
+--Q2
+INSERT INTO Rating(rID,mID,stars)
+SELECT (SELECT rID FROM Reviewer WHERE name = 'James Cameron') , mID, 5 FROM Movie
